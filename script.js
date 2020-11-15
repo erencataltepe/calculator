ce7c735a92ea5cb0be50e0096174e5250c69d48e
erencataltepe <erencataltepe@gmail.com>
2020-11-15 16:35:28 +0300

    Create displayValue function and add event listener for buttons.

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function divide(a, b) {
 function operate(a, b, operator) {
   return operator(a,b);
 }
+
+function displayValue(value) {
+  const displayArea = document.getElementById("display-area");
+  displayArea.textContent = value;
+  return value;
+}
+
+const numberButtons = document.querySelectorAll(".numbers");
+numberButtons.forEach((numberButton) => {
+  numberButton.addEventListener("click", (e) => {
+    displayValue(e.target.textContent);
+  })
+})
