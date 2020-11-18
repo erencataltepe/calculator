8b05491be172cc16f50494c2b6808fd951173635
erencataltepe <erencataltepe@gmail.com>
2020-11-18 20:33:15 +0300

    Work for calculations with two operands.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let currentOperator = null;
 let firstOperand = null;
 let secondOperand = null;
+let toBeCleaned = false;
 const displayArea = document.getElementById("display-area");
 const numberButtons = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operator");
+const equalSign = document.getElementById("equal");
+const clearButton = document.getElementById("clear");
 
 function add(a, b) {
   return a + b;
@@ -22,7 +25,20 @@ function divide(a, b) {
 }
 
 function operate(a, b, operator) {
-  return operator(a,b);
+  switch(operator) {
+    case "add":
+      return add(a, b);
+      break;
+    case "subtract":
+      return subtract(a, b);
+      break;
+    case "multiply":
+      return multiply(a, b);
+      break;
+    case "divide":
+      return divide(a, b);
+      break;
+  }
 }
 
 function displayValue(value) {
@@ -49,9 +65,20 @@ function clearDisplay() {
   displayArea.textContent = "";
 }
 
+function clearAllValues() {
+  firstOperand = null;
+  secondOperand = null;
+  currentOperator = null;
+  clearDisplay();
+}
+
 numberButtons.forEach((numberButton) => {
   numberButton.addEventListener('click', (e) => {    
+    if (toBeCleaned) {
+      clearDisplay();
+    }
     displayValue(e.target.textContent);
+    toBeCleaned = false;
   })
 })
 
@@ -59,6 +86,25 @@ operators.forEach((operator) => {
   operator.addEventListener('click', (e) => {
     setOperator(e.target.id);
     setOperand(getDisplayValue());
-    console.log(firstOperand);
+    toBeCleaned = true;
   })
+})
+
+equalSign.addEventListener("click", () => {
+  console.log(firstOperand);
+  console.log(secondOperand);
+  console.log(currentOperator);
+  console.log(toBeCleaned);
+  if (firstOperand && currentOperator && !toBeCleaned && !secondOperand) {
+    setOperand(getDisplayValue());
+    let result = operate(Number(firstOperand), Number(secondOperand), currentOperator);
+    console.log(result);
+    clearDisplay();
+    displayValue(result);
+  }
+})
+
+
+clearButton.addEventListener('click', () => {
+  clearAllValues();
 })
